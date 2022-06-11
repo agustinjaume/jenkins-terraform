d4084f40180d7d300c2eb31c62d879c05013b6ce
Armando <arman77mx@live.com.mx>
2022-06-11 17:58:58 -0500

    Update backend.tf
    cambio de nombre de bucket

--- bastion/backend.tf
+++ bastion/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket          = "tfstate-bucket-4291-ideasextraordinarias-default"
+    bucket          = "ghdz-grupo-bigdata-poc-tfstate"
     prefix          = "bastion/terraform.tfstate"
 
   }
