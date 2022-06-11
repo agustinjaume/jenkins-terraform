81e94dfb0cb4fd2ee5582690f0f7f21bed8cd55a
Armando <arman77mx@live.com.mx>
2022-06-11 18:17:44 -0500

    Update data.tf
    cambio de region

--- bastion/data.tf
+++ bastion/data.tf
@@ -10,10 +10,10 @@ data "google_compute_network" "vpc" {
 
 data "google_compute_subnetwork" "subnet-1" {
   name   = var.gcp_subnet_1  
-  region = var.gcp_zone
+  region = var.gcp_region
 }
 
 data "google_compute_zones" "available" {
-  region = var.gcp_zone  #"europe-west3" 
+  region = var.gcp_region  #"us-central1" 
   status = "UP"
 }
